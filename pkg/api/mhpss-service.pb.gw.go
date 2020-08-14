b740025aa6d72bcf44da7a4cf5fd481abb0a4b2d
Chin Jia Xiong <chinxiongwei@yahoo.com.my>
2020-08-15 01:58:10 +0800

    fix

--- pkg/api/mhpss-service.pb.gw.go
+++ pkg/api/mhpss-service.pb.gw.go
@@ -5466,26 +5466,19 @@ func local_request_MhpssService_ClientCreateMeeting_0(ctx context.Context, marsh
 
 }
 
+var (
+	filter_MhpssService_ClientTutorial_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_MhpssService_ClientTutorial_0(ctx context.Context, marshaler runtime.Marshaler, client MhpssServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq TutorialRequest
 	var metadata runtime.ServerMetadata
 
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["id"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-
-	protoReq.Id, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MhpssService_ClientTutorial_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.ClientTutorial(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -5497,22 +5490,8 @@ func local_request_MhpssService_ClientTutorial_0(ctx context.Context, marshaler 
 	var protoReq TutorialRequest
 	var metadata runtime.ServerMetadata
 
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["id"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
-	}
-
-	protoReq.Id, err = runtime.String(val)
-
-	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_MhpssService_ClientTutorial_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.ClientTutorial(ctx, &protoReq)
@@ -10398,7 +10377,7 @@ var (
 
 	pattern_MhpssService_ClientCreateMeeting_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "client", "meetings", "id"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_MhpssService_ClientTutorial_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "client", "tutorial", "id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_MhpssService_ClientTutorial_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "client", "tutorial"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
