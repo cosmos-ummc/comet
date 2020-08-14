551fa26343db5f4eeef7f2a2e6a349e609eff439
Chin Jia Xiong <chinxiongwei@yahoo.com.my>
2020-08-14 23:45:04 +0800

    wording

--- pkg/constants/message.go
+++ pkg/constants/message.go
@@ -9,16 +9,16 @@ var ReminderMessage = "Hi buddy, today is day 7 of quarantine, hope you are doin
 	"Can you type /dass command for DASS assessment and /iesr command for IES-R assessment? Really appreciate your efforts!"
 
 var PhoneLinkMessage = "Well, I will bring you around with all the tests I’ve prepared upon your first time login! Let’s get started. " +
-	"\nPlease follow my instruction step by step to have a better experience! Owh yeah, before we start the test, please let me know more about you so that we can arrange a better experience for you in QUARANTEAM! " +
-	"\nPlease click /pers to proceed."
+	"\n\nPlease follow my instruction step by step to have a better experience! Owh yeah, before we start the test, please let me know more about you so that we can arrange a better experience for you in QUARANTEAM! " +
+	"\n\nPlease click /pers to proceed."
 
 var FirstDassMessage = "We will have 5 simple and casual question for you daily! \nIt is important for us to know how is your psychological well-being." +
-	"\nPlease click /5Q to proceed."
+	"\n\nPlease click /5Q to proceed."
 
-var FirstDailyMessage = "Let’s try the last test shall we? \nClick /iesr to proceed!"
+var FirstDailyMessage = "Let’s try the last test shall we? \n\nClick /iesr to proceed!"
 
 var FirstIesrMessage = "Well it seems like you have experienced all the tests I prepared for you! It’s time for your to find more hidden gems from our Client portal! " +
-	"\nPlease login to https://quaranteams-main.herokuapp.com/ " +
-	"with the credentials below:\n" +
+	"\n\nPlease login to https://quaranteams-main.herokuapp.com/ " +
+	"with the credentials below:\n\n" +
 	"email: testuser@gmail.com\n" +
 	"password: test12345"
